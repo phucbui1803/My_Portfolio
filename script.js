8a1f437a245ab7376aa421229447c4058c5969e0
phucbui1803 <phucbui1803@gmail.com>
2025-09-10 16:49:58 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Khi click nút Hiển thị lời nhắn
+document.getElementById("contactBtn").addEventListener("click", function() {
+    document.getElementById("msg").textContent = "Cảm ơn bạn đã ghé thăm portfolio của tôi!";
+});
